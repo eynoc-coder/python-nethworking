e69be77aaf501ce4023945436aa097e5f0a24248
eric yann Nouyom Chekem <eric-yann.nouyom-chekem.1@ulaval.ca>
2020-11-10 13:42:22 -0500

    b

--- add_headers_user_agent.py
+++ add_headers_user_agent.py
@@ -3,6 +3,7 @@ from urllib.request import Request
 USER_AGENT = 'Mozilla/5.0 (Windows NT 5.1; rv:20.0) Gecko/20100101 Firefox/20.0'
 URL = 'http://www.debian.org'
 
+
 def add_header_user_agent():
     headers = {'Accept-Language' : 'nl', 'User-agent' : USER_AGENT}
     requests = Request(URL, headers=headers)
--- extract-links-from-a-url-with-urllib-using-regex.py
+++ extract-links-from-a-url-with-urllib-using-regex.py
@@ -3,7 +3,9 @@ import re
 
 
 def download_page(url):
-    return urlopen(url).read().decode('utf-8')
+    USER_AGENT = 'Mozilla/5.0 (Windows NT 5.1; rv:20.0) Gecko/20100101 Firefox/20.0'
+    headers = {'Accept-Language': 'nl', 'User-agent': USER_AGENT}
+    return urlopen(url, headers=headers).read().decode('utf-8')
 
 
 def extract_links(page):
